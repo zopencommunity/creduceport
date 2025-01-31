3505d876da6c1461e95ad91bf3875098f4e35a35
Kyle Huang <khuang2024@gmail.com>
2025-01-31 15:29:07 -0600

    Initial commit for creduce port.

---
+++ README.md
@@ -0,0 +1,5 @@
+[![Automatic version updates](https://github.com/zopencommunity/creduceport/actions/workflows/bump.yml/badge.svg)](https://github.com/zopencommunity/creduceport/actions/workflows/bump.yml)
+
+# creduce
+
+C-Reduce, a C and C++ program reducer
---
+++ cicd-dev.groovy
@@ -0,0 +1,14 @@
+node('linux')
+{
+  stage ('Poll') {
+    checkout([
+      $class: 'GitSCM',
+      branches: [[name: '*/main']],
+      doGenerateSubmoduleConfigurations: false,
+      extensions: [],
+      userRemoteConfigs: [[url: 'https://github.com/zopencommunity/creduceport.git']]])
+  }
+  stage('Build') {
+    build job: 'Port-Pipeline', parameters: [string(name: 'PORT_GITHUB_REPO', value: 'https://github.com/zopencommunity/creduceport.git'), string(name: 'PORT_DESCRIPTION', value: 'C-Reduce, a C and C++ program reducer' ), string(name: 'BUILD_LINE', value: 'DEV') ]
+  }
+}
